a6c445b795d6c0e325e87d5f4ebbb41e1c196401
nilo1509 <dmartinsgazzoli@gmail.com>
2024-09-01 18:48:57 -0300

    chore: dataclass

--- src/core/entities/voter.py
+++ src/core/entities/voter.py
@@ -1,4 +1,10 @@
+from dataclasses import dataclass
+
+class VoterData:
+    name: str
+    cardNumber: int
+
 class Voter:
-    def __init__(self, name, cardNumber):
-        self.__cardNumber = cardNumber;
-        self.__name = name;
+    def __init__(self, voter_data: VoterData):
+        self.__cardNumber = voter_data.cardNumber;
+        self.__name = voter_data.name;
