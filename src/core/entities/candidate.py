8044938f5455120735dfae2250689ec3fdfe3a37
nilo1509 <dmartinsgazzoli@gmail.com>
2024-09-10 15:55:27 -0300

    fix: candidate file remove aditional param in super method

--- src/core/entities/candidate.py
+++ src/core/entities/candidate.py
@@ -3,7 +3,7 @@ from core.entities.voter import Voter;
 
 class Candidate(Voter):
     def __init__(self, voter_data: DataVoter, vote_data: DataVote, candidate_data: DataCandidate):
-        super().__init__(voter_data, vote_data, vote_data);
+        super().__init__(voter_data, vote_data);
         self.candidateNumber = candidate_data.candidateNumber;
         self.candidateID = candidate_data.candidateID;
         self.profilePicture = candidate_data.profilePicture;
