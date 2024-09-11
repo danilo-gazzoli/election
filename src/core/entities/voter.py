47c4e9a7fc11c54d23eed596a452d922e4e99de6
Ojogotcc <ojogotcc24@gmail.com>
2024-09-11 08:42:45 -0300

    feat: creation of vote null and none getters and setters

--- src/core/entities/voter.py
+++ src/core/entities/voter.py
@@ -13,46 +13,63 @@ class Voter:
         
     # Voter card number getter and setter
     @property
-    def VoterCardNumber(self):
+    def cardNumber(self):
         return self._cardNumber;
     
-    @VoterCardNumber.setter
+    @cardNumber.setter
     def set_card_number(self, value: int):
         self._cardNumber = value;
         
     # voter name getter and setter
     @property
-    def VoterName(self):
+    def name(self):
         return self._name;
     
-    @VoterName.setter
+    @name.setter
     def set_name(self, value: str):
         self._name = value;
         
     # vote number getter and setter
     @property
-    def VoterVoteNumber(self):
+    def voteNumber(self):
         return self._voteNumber
     
-    @VoterVoteNumber.setter
-    def set_vote_number(self, value: int):
+    @voteNumber.setter
+    def set_vote_number(self, value):
         self._voteNumber = value;
         
     # vote permission getter and setter
     @property
-    def VoterVotePermission(self):
+    def votePermission(self):
         return self._votePermission
     
-    @VoterVotePermission.setter
+    @votePermission.setter
     def set_vote_permission(self, value: bool):
         self._votePermission = value;
         
     # vote position getter and setter
     @property
-    def VoterVotePosition(self):
+    def votePosition(self):
         return self._voteNumber
     
-    @VoterVotePosition.setter
+    @votePosition.setter
     def set_vote_position(self, value: str):
         self._votePosition = value;
     
+    # vote none getter and setter
+    @property
+    def voteNone(self):
+        return self._voteNone;
+    
+    @voteNone.setter
+    def set_voteNone(self, value):
+        self._voteNone = value;
+    
+    # vote null getter and setter
+    @property
+    def voteNull(self):
+        return self._voteNone;
+    
+    @voteNull.setter
+    def set_voteNull(self, value):
+        self._voteNull = value;
