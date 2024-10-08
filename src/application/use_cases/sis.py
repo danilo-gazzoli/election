96b6377449bcb2923de1ed958a150b5eeb854dc0
nilo1509 <dmartinsgazzoli@gmail.com>
2024-10-08 12:07:31 -0300

    feat: candidate manager interface

--- src/application/use_cases/sis.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-novo_caminho = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../'))
-print("Caminho adicionado:", novo_caminho)
-sys.path.append(novo_caminho)
-print("Caminhos no sys.path:")
-for caminho in sys.path:
-    print(caminho)
